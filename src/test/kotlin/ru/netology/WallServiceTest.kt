9a1afc0de8757dfa181ce943e60b6b724f0cebd4
AlexeYeS <m0use@mail.ru>
2022-09-11 22:45:27 +0300

    add test jacoco

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -9,18 +9,43 @@ class WallServiceTest {
     @Test
     fun add_post() {
         //arrange
-        var posts = emptyArray<Post>()
-        val expectedRes = Post(id = 0, ownerId = 1, fromId = 1, createdBy = 99, date = 20220611, text = "Hello world", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post)
+        val service = WallService
 
         //act
-        WallService.add(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220611, text = "Hello world", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val postNew = service.add(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220611, text = "Hello world", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
 
         //assert
-        assertEquals(expectedRes, posts[0])
+        assertTrue(postNew.id == 0)
     }
 
     @Test
-    fun update_post() {
+    fun update_postTrue() {
+        //arrange
+        val service = WallService
+        val postNew1 = service.add(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220611, text = "Hello world", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val postNew2 = service.add(Post(id = 2, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val postNew3 = service.add(Post(id = 3, ownerId = 1, fromId = 2, createdBy = 99, date = 20220613, text = "Hello Java", replyOwnerId = 2, replyPostId = 1, friends_only = false, postType = PostType.reply))
 
+
+        //act
+        val result = service.update(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+
+        //assert
+        assertTrue(result)
+    }
+    @Test
+    fun update_postFalse() {
+        //arrange
+        val service = WallService
+        val postNew1 = service.add(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220611, text = "Hello world", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val postNew2 = service.add(Post(id = 2, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val postNew3 = service.add(Post(id = 3, ownerId = 1, fromId = 2, createdBy = 99, date = 20220613, text = "Hello Java", replyOwnerId = 2, replyPostId = 1, friends_only = false, postType = PostType.reply))
+
+
+        //act
+        val result = service.update(Post(id = 4, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+
+        //assert
+        assertFalse(result)
     }
 }
