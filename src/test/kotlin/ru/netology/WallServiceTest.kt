5867a366a3e7e847f82fd5e9e3d3b0e15260f06a
AlexeYeS <m0use@mail.ru>
2022-09-12 23:30:20 +0300

    fix build.gradle

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -5,7 +5,6 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class WallServiceTest {
-
     @Test
     fun add_post() {
         //arrange
@@ -17,15 +16,13 @@ class WallServiceTest {
         //assert
         assertTrue(postNew.id == 0)
     }
-
     @Test
     fun update_postTrue() {
         //arrange
         val service = WallService
         val postNew1 = service.add(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220611, text = "Hello world", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
-        val postNew2 = service.add(Post(id = 2, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val postNew2 = service.add(Post(id = 2, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Motlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
         val postNew3 = service.add(Post(id = 3, ownerId = 1, fromId = 2, createdBy = 99, date = 20220613, text = "Hello Java", replyOwnerId = 2, replyPostId = 1, friends_only = false, postType = PostType.reply))
-
 
         //act
         val result = service.update(Post(id = 1, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
@@ -41,9 +38,8 @@ class WallServiceTest {
         val postNew2 = service.add(Post(id = 2, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
         val postNew3 = service.add(Post(id = 3, ownerId = 1, fromId = 2, createdBy = 99, date = 20220613, text = "Hello Java", replyOwnerId = 2, replyPostId = 1, friends_only = false, postType = PostType.reply))
 
-
         //act
-        val result = service.update(Post(id = 4, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Kotlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
+        val result = service.update(Post(id = 4, ownerId = 1, fromId = 1, createdBy = 99, date = 20220612, text = "Hello Potlin", replyOwnerId = 1, replyPostId = 1, friends_only = false, postType = PostType.post))
 
         //assert
         assertFalse(result)
